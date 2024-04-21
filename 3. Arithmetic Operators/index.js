b154e5d62484f0e6140817055d9eea23983c3216
Ritik Sharma <ritiksharma0405@gmail.com>
2024-04-21 16:09:34 +0530

    21-04-2024

---
+++ 3. Arithmetic Operators/index.js
@@ -0,0 +1,37 @@
+/* 
+Arithmetic operators = operants (value, variables, etc.)
+                       operators (+ - * /)
+                       ex. 11 = x + 5 */
+
+let students = 30;
+
+// students = students + 1;
+// students = students - 1;
+// students = students * 2;
+// students = students / 2;
+// students = students ** 3;           // Exponent
+// let extraStudents = students % 2;           // Remainder
+
+// students += 1;
+// students -= 1;
+// students *= 2;
+// students /= 2;
+// students **= 2;
+// students %= 2;
+
+// increment and decrement operator
+// students++;
+// students--;
+
+// console.log(students);
+
+/* Operator Precedence
+    1. parenthesis ()
+    2. exponents
+    3. multiplication & division & modulo
+    4. addition & subtraction
+*/
+
+let result = 1 + 2 * 3 + 4 ** 2;
+
+console.log(result);
