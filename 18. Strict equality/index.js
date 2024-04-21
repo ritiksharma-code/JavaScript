6ee99a4f70f6b77c3b1b2ce96e567a89d2bc8e56
Ritik Sharma <ritiksharma0405@gmail.com>
2024-04-21 16:12:50 +0530

    21-04-2024

---
+++ 18. Strict equality/index.js
@@ -0,0 +1,15 @@
+/*  = assignment operator
+    == comparison operator (compare if values are equal)
+    === strict equality operator (compare if values & datatypes are equal)
+    != inequality operator
+    !== strict inequality operator
+*/
+
+const PI = 3.14;
+
+if(PI === "3.14"){              // not same datatype so condition is false
+    console.log("That is PI.");
+}
+else{
+    console.log("That is not PI.");
+}
