4d619c927cf1f77ecd8ebd59b7f86fc5b4702c07
Ritik Sharma <ritiksharma0405@gmail.com>
2024-04-21 16:13:12 +0530

    21-04-2024

---
+++ 20. For loops/index.js
@@ -0,0 +1,22 @@
+// for loops = repeat some code a LIMITED amount of times
+
+/* for(let i = 0; i <= 10; i+=2){
+    console.log(i);
+} */
+
+/* for(let i = 10; i > 0; i--){
+    console.log(i);
+}
+console.log("HAPPY NEW YEAR!"); */
+
+for(let i = 1; i<=20; i++)
+{
+    if(i == 13){
+        // continue;
+        break;
+    }
+    else{
+        console.log(i);
+    }
+}
+    
