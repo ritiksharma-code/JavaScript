b7c6a2273a5222c6b942c956f319df89f1fd62e5
Ritik Sharma <ritiksharma0405@gmail.com>
2024-04-21 16:08:49 +0530

    21-04-2024

---
+++ 1. Basics/index.js
@@ -0,0 +1,14 @@
+/* console.log(`Hello World`);
+console.log(`I like pizza`);
+
+window.alert(`This is an alert`);
+window.alert(`I like pizza`); */
+
+// This is a singleline comment
+
+/*
+    This is a multiline comment
+*/
+
+document.getElementById("myH1").textContent = `Hello!`;
+document.getElementById("myP").textContent = `I like pizza!`;
