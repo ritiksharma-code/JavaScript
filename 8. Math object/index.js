cb126a1c6ea99e1b493895ba24d42648e16db819
Ritik Sharma <ritiksharma0405@gmail.com>
2024-04-21 16:10:43 +0530

    21-04-2024

---
+++ 8. Math object/index.js
@@ -0,0 +1,28 @@
+// Math - built-in object that provides a collection of properties and methods
+
+/* Math.PI;
+
+console.log(Math.PI);
+console.log(Math.E); */
+
+let x = 3;
+let y = 2;
+let z = 1;
+
+// z = Math.round(x);       // round off
+// z = Math.floor(x);       // floor value
+// z = Math.ceil(x);        // ceiling value
+// z = Math.trunc(x);       // truncate - eliminate any decimal portion
+// z = Math.pow(x, y);      // power/exponent - x^y    
+// z = Math.sqrt(81);       // Squareroot
+// z = Math.log(x);         // logarithmic value
+// z = Math.sin(x);         // sine value
+// z = Math.cos(x);         // cosine value
+// z = Math.tan(x);         // tangent value
+// z = Math.abs(x);         // absolute value - makes it positive
+// z = Math.sign(x);        // sign of the number/variable (-1, 0, 1)
+
+let max = Math.max(x, y, z);     // maximum value
+let min = Math.min(x, y, z);     // minimum value
+
+console.log(min);
