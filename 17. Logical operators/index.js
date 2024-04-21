3dd200e4251ea9a99b5f516517b99380ae97fead
Ritik Sharma <ritiksharma0405@gmail.com>
2024-04-21 16:12:40 +0530

    21-04-2024

---
+++ 17. Logical operators/index.js
@@ -0,0 +1,32 @@
+/* logical operators = used to combine or manipulate boolean values (true or false)
+                        
+                        AND = &&
+                        OR = ||
+                        NOT = !
+
+*/
+
+/* const temp = 20;
+
+if(temp > 0 && temp <= 30){
+    console.log("The weather is good!");
+}
+else{
+    console.log("The weather is bad!");
+}
+
+if(temp <= 0 || temp > 30){
+    console.log("The weather is bad!");
+}
+else{
+    console.log("The weather is good!");
+} */
+
+const isSunny = true;
+
+if(!isSunny){
+    console.log("It is Cloudy!");
+}
+else{
+    console.log("It is Sunny!");
+}
