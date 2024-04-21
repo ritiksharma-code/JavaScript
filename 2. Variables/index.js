c361c74efb648d5058d1d53a89e4aabb83eb849c
Ritik Sharma <ritiksharma0405@gmail.com>
2024-04-21 16:09:20 +0530

    21-04-2024

---
+++ 2. Variables/index.js
@@ -0,0 +1,48 @@
+/* Variable - A container that stores a value. Behaves as if it were the value it contains.
+
+1. declaration  let x;
+2. assignment   x = 100;
+
+*/
+
+/* let x = 123;
+
+console.log(x) */
+
+/* let age = 20;
+let price = 10.99;
+let gpa = 2.1;
+
+let firstName = "Ritik";        // String
+let favouriteFood = "Pizza"
+console.log(typeof firstName);
+console.log(`Your first name is: ${firstName}.`);
+
+console.log(typeof favouriteFood);
+console.log(`Your like ${favouriteFood}.`);
+
+let email = "ritiksharma0405@gmail.com";
+console.log(`Your email is: ${email}`)
+
+console.log(typeof age);
+console.log(typeof price);
+console.log(typeof gpa);
+
+console.log(`You are ${age} years old.`);
+console.log(`The price is $${price}.`);
+console.log(`Your GPA is: ${gpa}.`);
+
+// Booleans
+let online = false;      // true or false
+console.log(typeof online);
+console.log(`Ritik is online: ${online}.`);
+let forsale = true;
+console.log(`Is this car for sale: ${forsale}.`); */
+
+let fullName = "Ritik Sharma";
+let age = 20;
+let student = true;
+
+document.getElementById("p1").textContent = `Your name is ${fullName}.`;
+document.getElementById("p2").textContent = `Your are ${age} years old.`;
+document.getElementById("p3").textContent = `Enrolled: ${student}.`;
