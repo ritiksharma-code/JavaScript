95fe481a2223f133273f7722bc20f6c8481b12bc
Ritik Sharma <ritiksharma0405@gmail.com>
2024-05-14 23:13:55 +0530

    14-05-2024

---
+++ 30. callback/index.js
@@ -0,0 +1,10 @@
+/* callback = a function is passed as an argument to another function.
+              
+              used to handle asynchronous operations:
+              1. Reading a file
+              2. Networdk requests
+              3. Interacting with databases
+              
+              "Hey, when you're done, call this next.
+*/
+
