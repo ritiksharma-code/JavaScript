2db87373160a5cbe7fbf99794ee8c7cd674e0ad4
Ritik Sharma <ritiksharma0405@gmail.com>
2024-07-18 20:06:18 +0530

    18-07-2024

--- 34. reduce method/index.js
+++ 34. reduce method/index.js
@@ -2,3 +2,31 @@
     .reduce() = reduce the elements of a array to a single value
 */
 
+/* const prices = [5, 10, 30, 25, 15, 20];
+
+const total = prices.reduce(sum);
+
+console.log(`$${total.toFixed(2)}`);
+
+function sum(previous, next)
+{
+    return previous + next;
+} */
+
+const grades = [75, 50, 90, 80, 65, 95];
+
+const maximum = grades.reduce(getMax);
+const minimum = grades.reduce(getMin);
+
+console.log(maximum);
+console.log(minimum);
+
+function getMax(accumulator, element)
+{
+    return Math.max(accumulator, element);
+}
+
+function getMin(accumulator, element)
+{
+    return Math.min(accumulator, element);
+}
---
+++ 35. Function expressions/index.js
@@ -0,0 +1,6 @@
+/* 
+    function declaration = define a reusable block of code that performs a specific task
+
+    function expression = a way to define functions as values fo variables
+*/
+
