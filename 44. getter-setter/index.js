ab632303aa69beec849a3fc1bea3ec2779e7f03e
Ritik Sharma <ritiksharma0405@gmail.com>
2024-07-27 21:40:43 +0530

    27-07-2024

--- 44. getter-setter/index.js
+++ 44. getter-setter/index.js
@@ -5,7 +5,7 @@
     validate and modify a value when reading/writing a property.
 */
 
-class Rectangle{
+/* class Rectangle{
     constructor(width, height){
         this.width = width;
         this.height = height;
@@ -31,24 +31,24 @@ class Rectangle{
 
     get width(){
         // return this._width;
-        return this._width.toFixed(1);
+        return `${this._width.toFixed(1)}cm`;
     }
     get height(){
         // return this._height;
-        return this._height.toFixed(1);
+        return `${this._height.toFixed(1)}cm`;
     }
 
     get area(){
         // return this._width * this._height;
-        return (this._width * this._height).toFixed(1);
+        return `${(this._width * this._height).toFixed(1)}cm^2`;
     }
 }
 
 // const rectangle = new Rectangle(-1000000, "pizza");
 const rectangle = new Rectangle(3, 4);
 
-/* rectangle.width = -1000000;
-rectangle.height = "pizze"; */
+// rectangle.width = -1000000;
+// rectangle.height = "pizze";
 
 rectangle.width = 5;
 rectangle.height = 6;
@@ -56,3 +56,58 @@ rectangle.height = 6;
 console.log(rectangle.width);
 console.log(rectangle.height);
 console.log(rectangle.area);
+ */
+
+class Person{
+    constructor(firstName, lastName, age){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    set firstName(newFirstName){
+        if(typeof newFirstName === "string" && newFirstName.length > 0){
+            this._firstName = newFirstName;
+        }
+        else{
+            console.error("First name must be a non-empty string.");
+        }
+    }
+    set lastName(newLastName){
+        if(typeof newLastName === "string" && newLastName.length > 0){
+            this._lastName = newLastName;
+        }
+        else{
+            console.error("Last name must be a non-empty string.");
+        }
+    }
+    set age(newAge){
+        if(typeof newAge === "number" && newAge >= 0){
+            this._age = newAge;
+        }
+        else{
+            console.error("Age must be a positive number.");
+        }
+    }
+
+    get firstName(){
+        return this._firstName;
+    }
+    get lastName(){
+        return this._lastName;
+    }
+    get fullName(){
+        return this._firstName + " " + this._lastName;
+    }
+    get age(){
+        return this._age;
+    }
+}
+
+// const person = new Person(420, 60, "pizza");
+const person = new Person("Ritik", "Sharma", 20);
+
+console.log(person.firstName);
+console.log(person.lastName);
+console.log(person.age);
+console.log(person.fullName);
---
+++ 45. destructuring/index.js
@@ -0,0 +1,8 @@
+/* 
+    destructuring = extract values from arrays and objects,
+                    then assigh them to variables in a convenient way
+                    [] = to perform array destructuring
+                    {} = to perform object destructuring
+                    5 examples
+*/
+
