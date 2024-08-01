0542c07321152920af497e44ec487c2632700791
Ritik Sharma <ritiksharma0405@gmail.com>
2024-08-01 15:40:03 +0530

    01-08-2024

--- 47. Array of Objects/index.js
+++ 47. Array of Objects/index.js
@@ -1,2 +1,8 @@
 // Array of Objects
 
+const fruits = [{name: "apple", color: "red", calories: 95}, 
+                {name: "orange", color: "orange", calories: 45}, 
+                {name: "banana", color: "yellow", calories: 105}, 
+                {name: "coconut", color: "white", calories: 159}, 
+                {name: "pineapple", color: "yellow", calories: 37}]
+
