e2e33ea07a658df4ba2536e3a0740e5b6e66d0cc
ritiksharma-code <ritiksharma0405@gmail.com>
2024-09-12 17:22:08 +0530

    12-09-2024

--- 64. Key events/index.js
+++ 64. Key events/index.js
@@ -1,5 +1,47 @@
 /* 
     eventListener = Listen for specific events to create interactive web pages
-                    events: keydown, 
+                    events: keydown, keypress
                     .addEventListener(event, (arrow function/callback/anonymous function));
 */
+
+const myBox = document.getElementById("myBox");
+const moveAmount = 30;
+let x = 0;
+let y = 0;
+
+document.addEventListener("keydown", (event)=>{
+    if(event.key.startsWith("Arrow")){
+        event.preventDefault();
+
+        switch(event.key){
+            case "ArrowUp":
+                y -= moveAmount;
+                break
+            case "ArrowDown":
+                y += moveAmount;
+                break;
+            case "ArrowLeft":
+                x -= moveAmount;
+                break;
+            case "ArrowRight":
+                x += moveAmount;
+                break;
+        }
+        myBox.style.top = `${y}px`;
+        myBox.style.left = `${x}px`;
+    }
+})
+
+document.addEventListener("keydown", (event)=>{
+    // console.log(event);
+    // console.log(event.key);
+    // console.log(`Key down = ${event.key}`);
+    myBox.textContent = "😮";
+    myBox.style.backgroundColor = "tomato";
+});
+
+document.addEventListener("keyup", (event)=>{
+    // console.log(`Key up ${event.key}`);
+    myBox.textContent = "😃";
+    myBox.style.backgroundColor = "lightblue";
+});
