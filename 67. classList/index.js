4816479d1fa5578a7090c6227f7695484b9688e7
ritiksharma-code <ritiksharma0405@gmail.com>
2024-09-12 21:30:54 +0530

    12-09-2024

--- 67. classList/index.js
+++ 67. classList/index.js
@@ -11,3 +11,78 @@
     contains()
 */
 
+const myH1 = document.getElementById("myH1");
+const myButton = document.getElementById("myButton");
+
+// ================ add() ================
+// myButton.classList.add("enabled");
+
+// ================ remove() ================
+// myButton.classList.remove("enabled");
+
+// ================ toggle() ================
+/* myButton.addEventListener("mouseover", (event)=>{
+    // event.target.classList.add("hover");
+    event.target.classList.toggle("hover");
+});
+myButton.addEventListener("mouseout", (event)=>{
+    // event.target.classList.remove("hover");
+    event.target.classList.toggle("hover");
+}); */
+
+// ================ replace() ================
+/* myButton.classList.add("enabled");
+myButton.addEventListener("click", (event)=>{
+    event.target.classList.replace("enabled", "disabled");
+}); */
+
+// ================ contains() ================
+/* myH1.classList.add("enabled");
+myButton.classList.add("enabled");
+myH1.addEventListener("click", (event)=>{
+    if(event.target.classList.contains("disabled")){
+        event.target.textContent += "🤬";
+    }
+    else{
+        event.target.classList.replace("enabled", "disabled");
+    }
+});
+myButton.addEventListener("click", (event)=>{
+    if(event.target.classList.contains("disabled")){
+        event.target.textContent += "🤬";
+    }
+    else{
+        event.target.classList.replace("enabled", "disabled");
+    }
+}); */
+
+let buttons = document.querySelectorAll(".myButtons");
+buttons.forEach((button)=>{
+    button.classList.add("enabled");
+});
+/* buttons.forEach((button)=>{
+    button.classList.remove("enabled");
+}); */
+
+buttons.forEach((button)=>{
+    button.addEventListener("mouseover", (event)=>{
+        event.target.classList.toggle("hover");
+    });
+});
+buttons.forEach((button)=>{
+    button.addEventListener("mouseout", (event)=>{
+        event.target.classList.toggle("hover");
+    });
+});
+buttons.forEach((button)=>{
+    button.addEventListener("click", (event)=>{
+        if(event.target.classList.contains("disabled")){
+            // event.target.classList.replace("disabled", "enabled");
+            event.target.textContent += '🤬';
+        }
+        else{
+            event.target.classList.replace("enabled", "disabled");
+        }
+        
+    });
+});
---
+++ 70. Callback Hell/index.js
@@ -0,0 +1,8 @@
+/* 
+    Callback Hell = SItuation in JavaScript where callbacks are nested
+                    within other callbacks to the degree where the code
+                    is difficult to read. Old pattern to handle asynchronous
+                    functions.
+                    Use Promises + aynch/await to avoid Callback Hell
+*/
+
